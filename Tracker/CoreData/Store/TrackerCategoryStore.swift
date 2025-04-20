c17c34b88a79a2d2dd0423d281d6c9ea4a4a4a38
Olya <>
2025-04-20 17:18:08 +0500

    созданы классы для работы с CoreData

---
+++ Tracker/CoreData/Store/TrackerCategoryStore.swift
@@ -0,0 +1,8 @@
+//
+//  TrackerCategoryStore.swift
+//  Tracker
+//
+//  Created by Olya on 19.04.2025.
+//
+
+import Foundation
---
+++ Tracker/CoreData/Transformers/Extension.swift
@@ -0,0 +1,8 @@
+//
+//  Extension.swift
+//  Tracker
+//
+//  Created by Olya on 19.04.2025.
+//
+
+import Foundation
