897a434719b21cae09974b5c3e8a3c34b1cfc73d
Olya <>
2025-05-18 23:54:14 +0500

    исправлены мелкие баги

--- Tracker/Tracker/Views/TrackerVC/TrackerCell.swift
+++ Tracker/Tracker/Views/TrackerVC/TrackerCell.swift
@@ -10,7 +10,7 @@ import UIKit
 
 protocol TrackerCellDelegate: AnyObject {
     func completeTracker(id: UUID, at indexPath: IndexPath)
-    func uncompletedTracker(id:UUID, at indexPath: IndexPath)
+    func uncompletedTracker(id: UUID, at indexPath: IndexPath)
 }
 
 final class TrackerCell: UICollectionViewCell {
@@ -48,10 +48,32 @@ final class TrackerCell: UICollectionViewCell {
         return label
     }()
     
+    private lazy var pinImage: UIImageView = {
+        let image = UIImageView()
+        image.image = .pin
+        image.isHidden = true
+        image.contentMode = .scaleAspectFit
+        image.translatesAutoresizingMaskIntoConstraints = false
+        return image
+    }()
+    
+    private lazy var emojiAndPinContainer: UIStackView = {
+        let spacer = UIView()
+        spacer.setContentHuggingPriority(.defaultLow, for: .horizontal)
+        
+        let stack = UIStackView(arrangedSubviews: [trackerCardEmojiLabel, spacer, pinImage])
+        
+        trackerCardEmojiLabel.widthAnchor.constraint(equalToConstant: 24).isActive = true
+            trackerCardEmojiLabel.heightAnchor.constraint(equalToConstant: 24).isActive = true
+        stack.axis = .horizontal
+        stack.alignment = .center
+        return stack
+    }()
+    
     private lazy var trackerCardContentStack: UIStackView = {
-        let stack = UIStackView(arrangedSubviews: [trackerCardEmojiLabel, trackerCardNameLabel])
+        let stack = UIStackView(arrangedSubviews: [emojiAndPinContainer, trackerCardNameLabel])
         stack.axis = .vertical
-        stack.alignment = .leading
+        stack.alignment = .fill
         stack.spacing = 8
         return stack
     }()
@@ -64,13 +86,6 @@ final class TrackerCell: UICollectionViewCell {
         return button
     }()
     
-    private lazy var pinImage: UIImageView = {
-       let image = UIImageView()
-        image.image = .pin
-        image.isHidden = true
-        return image
-    }()
-    
     private lazy var daysCounterLabel: UILabel = {
         let label = UILabel()
         let labelText = NSLocalizedString("tracker.day", comment: "")
@@ -80,60 +95,59 @@ final class TrackerCell: UICollectionViewCell {
         return label
     }()
     
-    @objc private func didTapTrackerButton(){
+    @objc private func didTapTrackerButton() {
         guard let id = trackerId, let indexPath = indexPath else {
             assertionFailure("no trackerId")
             return
         }
-        isCompletedToday ? delegate?.uncompletedTracker(id: id, at: indexPath) : delegate?.completeTracker(id: id, at: indexPath)
+        isCompletedToday
+            ? delegate?.uncompletedTracker(id: id, at: indexPath)
+            : delegate?.completeTracker(id: id, at: indexPath)
     }
     
-    private func setupUI(){
-        [trackerCardView, trackerButton, daysCounterLabel, pinImage].forEach {
+    private func setupUI() {
+        [trackerCardView, trackerButton, daysCounterLabel].forEach {
             $0.translatesAutoresizingMaskIntoConstraints = false
             contentView.addSubview($0)
         }
-
+        
         trackerCardContentStack.translatesAutoresizingMaskIntoConstraints = false
         trackerCardView.addSubview(trackerCardContentStack)
-
+        
         NSLayoutConstraint.activate([
             trackerCardView.topAnchor.constraint(equalTo: contentView.topAnchor),
             trackerCardView.heightAnchor.constraint(equalToConstant: 90),
             trackerCardView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
             trackerCardView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
-
+            
             trackerCardContentStack.topAnchor.constraint(equalTo: trackerCardView.topAnchor, constant: 12),
             trackerCardContentStack.leadingAnchor.constraint(equalTo: trackerCardView.leadingAnchor, constant: 12),
-            trackerCardContentStack.trailingAnchor.constraint(lessThanOrEqualTo: trackerCardView.trailingAnchor, constant: -12),
+            trackerCardContentStack.trailingAnchor.constraint(equalTo: trackerCardView.trailingAnchor, constant: -12),
             trackerCardContentStack.bottomAnchor.constraint(lessThanOrEqualTo: trackerCardView.bottomAnchor, constant: -12),
-
+            
+            pinImage.widthAnchor.constraint(equalToConstant: 8),
+            pinImage.heightAnchor.constraint(equalToConstant: 12),
+            
             trackerButton.heightAnchor.constraint(equalToConstant: 34),
             trackerButton.widthAnchor.constraint(equalToConstant: 34),
             trackerButton.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
             trackerButton.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -16),
-
+            
             daysCounterLabel.centerYAnchor.constraint(equalTo: trackerButton.centerYAnchor),
             daysCounterLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 12),
-            
-            pinImage.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 18),
-            pinImage.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -12)
         ])
     }
-
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupUI()
     }
     
-    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    func configureCell(tracker: Tracker, isCompletedToday: Bool, completedDays:Int, indexPath: IndexPath, isPinned: Bool){
-        
+    func configureCell(tracker: Tracker, isCompletedToday: Bool, completedDays: Int, indexPath: IndexPath, isPinned: Bool) {
         self.trackerId = tracker.id
         self.isCompletedToday = isCompletedToday
         self.indexPath = indexPath
@@ -148,24 +162,22 @@ final class TrackerCell: UICollectionViewCell {
             trackerButton.setImage(image, for: .normal)
             trackerButton.tintColor = tracker.color
         }
+        
         pinImage.isHidden = !isPinned
-        print("tracker.color: \(tracker.color)")
-
     }
     
     private func pluralizeDays(_ count: Int) -> String {
-            let remainder10 = count % 10
-            let remainder100 = count % 100
-            if remainder10 == 1 && remainder100 != 11 {
-                let text = NSLocalizedString("tracker.day", comment: "")
-                return "\(count) \(text)"
-            } else if remainder10 >= 2 && remainder10 <= 4 && (remainder100 < 10 || remainder100 >= 2) {
-                let text = NSLocalizedString("tracker.2,3,4day", comment: "")
-                return "\(count) \(text)"
-            } else {
-                let text = NSLocalizedString("tracker.days", comment: "")
-                return "\(count) \(text)"
-            }
+        let remainder10 = count % 10
+        let remainder100 = count % 100
+        if remainder10 == 1 && remainder100 != 11 {
+            let text = NSLocalizedString("tracker.day", comment: "")
+            return "\(count) \(text)"
+        } else if remainder10 >= 2 && remainder10 <= 4 && (remainder100 < 10 || remainder100 >= 20) {
+            let text = NSLocalizedString("tracker.2,3,4day", comment: "")
+            return "\(count) \(text)"
+        } else {
+            let text = NSLocalizedString("tracker.days", comment: "")
+            return "\(count) \(text)"
         }
-    
+    }
 }
